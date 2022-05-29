6ed79c0ab6c408d1e16d62a2ee9319350e1c2d10
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-05-29 14:54:46 +0300

    Add realisation interface Calculate

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class Calculator implements Calculate {
+
+    public static final BigDecimal MONTHS_IN_YEAR = new BigDecimal(12);
+    public static final BigDecimal PRECENTS = new BigDecimal(100);
+
+    @Override
+    public BigDecimal monthPay(BigDecimal years, BigDecimal sum, BigDecimal precent) {
+        return allSum(years, sum, precent).divide(MONTHS_IN_YEAR.multiply(years), MathContext.DECIMAL32).setScale(2, RoundingMode.DOWN);
+    }
+
+    @Override
+    public BigDecimal allSum(BigDecimal years, BigDecimal sum, BigDecimal precent) {
+        return sum.add(overPay(years, sum, precent)).setScale(2, RoundingMode.DOWN);
+    }
+
+    @Override
+    public BigDecimal overPay(BigDecimal years, BigDecimal sum, BigDecimal precent) {
+        return sum.divide(PRECENTS, MathContext.DECIMAL32).multiply(precent.multiply(years)).setScale(2, RoundingMode.DOWN);
+    }
+}
--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class CalculateTest {
 
-    public static Calculate calculate = null; // Здесь будет экземпляр класса-реализации
+    public static Calculate calculate = new Calculator(); // Здесь будет экземпляр класса-реализации
 
     @ParameterizedTest
     @MethodSource("argumentsMonthPayTest")
@@ -32,26 +32,26 @@ public class CalculateTest {
     }
 
     public static Stream<Arguments> argumentsMonthPayTest() {
-        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal(1000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal(500)),
-                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal(300)),
-                Arguments.of(new BigDecimal(0.5), new BigDecimal(10000), new BigDecimal(40), new BigDecimal(2000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal(12.5), new BigDecimal(93.75)));
+        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal("1000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal("500.00")),
+                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal("300.00")),
+                Arguments.of(new BigDecimal("0.5"), new BigDecimal(10000), new BigDecimal(40), new BigDecimal("2000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal("12.5"), new BigDecimal("93.75")));
     }
 
     public static Stream<Arguments> argumentsAllSumTest() {
-        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal(12000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal(6000)),
-                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal(7200)),
-                Arguments.of(new BigDecimal(0.5), new BigDecimal(10000), new BigDecimal(40), new BigDecimal(12000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal(12.5), new BigDecimal(1125)));
+        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal("12000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal("6000.00")),
+                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal("7200.00")),
+                Arguments.of(new BigDecimal("0.5"), new BigDecimal(10000), new BigDecimal(40), new BigDecimal("12000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal("12.5"), new BigDecimal("1125.00")));
     }
 
     public static Stream<Arguments> argumentsOverPayTest() {
-        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal(2000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal(1000)),
-                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal(1200)),
-                Arguments.of(new BigDecimal(0.5), new BigDecimal(10000), new BigDecimal(40), new BigDecimal(2000)),
-                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal(12.5), new BigDecimal(125)));
+        return Stream.of(Arguments.of(new BigDecimal(1), new BigDecimal(10000), new BigDecimal(20), new BigDecimal("2000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(5000), new BigDecimal(20), new BigDecimal("1000.00")),
+                Arguments.of(new BigDecimal(2), new BigDecimal(6000), new BigDecimal(10), new BigDecimal("1200.00")),
+                Arguments.of(new BigDecimal("0.5"), new BigDecimal(10000), new BigDecimal(40), new BigDecimal("2000.00")),
+                Arguments.of(new BigDecimal(1), new BigDecimal(1000), new BigDecimal("12.5"), new BigDecimal("125.00")));
     }
 }
